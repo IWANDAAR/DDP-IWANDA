1fe9f45c12d5f412153ef4ab4c893f49c23567e0
IWANDA AULIA ROSANDO <114456685+IWANDAAR@users.noreply.github.com>
2022-11-30 19:18:04 +0700

    Add files via upload

---
+++ PRAKTIKUM DDP WANDA/PERTEMUAN 1/Variabel.py
@@ -0,0 +1,8 @@
+nama = "Aldi" # string
+umur = 20 # interger
+isStudent = True # boolean
+ip = 3.50 # float
+
+
+
+print("Nama saya", nama, '. Nama saya 20 tahun mendatang adalah' , umur20thnMendatang)
